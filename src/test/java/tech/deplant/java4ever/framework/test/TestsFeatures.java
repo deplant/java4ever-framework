0cecf612eb9e756865434105d83af95b5c298b2e
Cassandrus <cassandrus@protonmail.com>
2022-06-16 19:14:47 +0300

    Little fixes

--- src/test/java/tech/deplant/java4ever/framework/test/TestsFeatures.java
+++ src/test/java/tech/deplant/java4ever/framework/test/TestsFeatures.java
@@ -1,15 +1,8 @@
 package tech.deplant.java4ever.framework.test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
-import tech.deplant.java4ever.binding.GraphQL;
-import tech.deplant.java4ever.binding.Net;
 import tech.deplant.java4ever.binding.loader.JavaLibraryPathLoader;
 import tech.deplant.java4ever.framework.Credentials;
 import tech.deplant.java4ever.framework.Sdk;
@@ -19,12 +12,12 @@ import tech.deplant.java4ever.framework.contract.EverOSGiver;
 import tech.deplant.java4ever.framework.template.MsigTemplate;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Log4j2
 public class TestsFeatures {
+
+    public static final String TEST_SERVER = "http://80.78.241.3/";
 
 //    public static Account ofAddress(Sdk sdk, tech.deplant.java4ever.framework.type.Address address, ContractAbi abi) throws Sdk.SdkException {
 //        Map<String, Object> filter = new HashMap<>();
@@ -40,7 +33,7 @@ public class TestsFeatures {
                 .networkEndpoints(SdkBuilder.Network.DEV_NET.endpoints())
                 .create(new JavaLibraryPathLoader());
         final Sdk sdkSE = new SdkBuilder()
-                .networkEndpoints(new String[]{"http://80.78.241.3/"})
+                .networkEndpoints(new String[]{TEST_SERVER})
                 .timeout(50L)
                 .create(new JavaLibraryPathLoader());
 
@@ -55,38 +48,5 @@ public class TestsFeatures {
 
     }
 
-    @Test
-    public void testJacksonConvert() throws ExecutionException, InterruptedException {
-        final Sdk sdk = new SdkBuilder()
-                .networkEndpoints(new String[]{"http://80.78.254.199/"})
-                .timeout(50L)
-                .create(new JavaLibraryPathLoader());
-
-        ObjectMapper mapper = JsonMapper.builder()
-                .addModule(new ParameterNamesModule())
-                .addModule(new Jdk8Module())
-                .addModule(new JavaTimeModule())
-                .build();
-
-        record AccountQuery(String id, int acc_type, String balance, String boc, long last_paid) {
-        }
-
-        Map<String, Object> filter = new HashMap<>();
-        filter.put("id", new GraphQL.Filter.In(new String[]{"0:e2a1dcec8bebff29c207d8944aef1bc8a5a9500789096c6a83a3a9bd71dd75fa"}));
-        Object[] results = Net.queryCollection(sdk.context(), "accounts", filter, "id acc_type balance boc last_paid", null, null).get().result();
-        var query = mapper.convertValue(results[0], AccountQuery.class);
-
-        log.debug(query.id() + ", " + query.balance());
-    }
-
-    @Test
-    public void testParallelSdkExecution() {
-
-    }
-
-    @Test
-    public void testSequentialSdkExecution() throws ExecutionException, InterruptedException {
-
-    }
 
 }
